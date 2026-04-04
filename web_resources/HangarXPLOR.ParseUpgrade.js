7ec2635329581ae19a389eb3cb423350b7d4a0bb
Dwayde <klnpotap@gmail.com>
2026-04-04 21:19:44 +0300

    Improvments for upgrades

--- web_resources/HangarXPLOR.ParseUpgrade.js
+++ web_resources/HangarXPLOR.ParseUpgrade.js
@@ -1,5 +1,5 @@
 
-var HangarXPLOR = HangarXPLOR || {};
+/*var HangarXPLOR = HangarXPLOR || {};
 
 HangarXPLOR.ParseUpgrade = function(pledgeName)
 {
@@ -38,6 +38,39 @@ HangarXPLOR.ParseUpgrade = function(pledgeName)
           }
         }
       } catch (e) { }
+    }
+  }
+}*/
+
+var HangarXPLOR = HangarXPLOR || {};
+
+HangarXPLOR.ParseUpgrade = function()
+{
+  this.filters.is_upgrade = $('.js-upgrade-data', this).length > 0;
+  
+  if (this.filters.is_upgrade)
+  {
+    this.upgrade_data = JSON.parse($('.js-upgrade-data', this).val());
+
+    $('.title:contains(Upgrade)', this).after('<div class="kind">Upgrade</div>');
+
+    HangarXPLOR._upgradeCount += 1;
+
+    this.displayName = this.upgrade_data.match_items[0].name + ' to ' + this.upgrade_data.target_items[0].name
+
+    var target_name = this.upgrade_data.target_items[0].name;
+    var i, j;
+    for (i = 0, j = HangarXPLOR._shipMatrix.length; i < j; i++) {
+      if (target_name.toLowerCase().indexOf(HangarXPLOR._shipMatrix[i].name.toLowerCase()) > -1) {
+
+        HangarXPLOR.Log('Matched', HangarXPLOR._shipMatrix[i].name, 'in', target_name);
+
+        target_name = (HangarXPLOR._shipMatrix[i].displayName || HangarXPLOR._shipMatrix[i].name);
+        if (HangarXPLOR._shipMatrix[i].thumbnail != undefined) {
+          $('.basic-infos .image', this).css({ 'background-image': 'url("' + HangarXPLOR._shipMatrix[i].thumbnail + '")'});
+        }
+        break;
+      }
     }
   }
 }
